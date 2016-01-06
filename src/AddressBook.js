3d14a5a2a95a8f4910cbb7ebad202d53d76c6994
Natalie Chin <natalie.j.chin@gmail.com>
2016-01-05 19:25:57 -0600

    first commit with two passing specs

---
+++ src/AddressBook.js
@@ -0,0 +1,16 @@
+function AddressBook() {
+	this.contacts = [];
+}
+
+AddressBook.prototype.addContact = function(contact) {
+	this.contacts.push(contact);
+}
+
+AddressBook.prototype.getContact = function(index) {
+	return this.contacts[index];
+}
+
+AddressBook.prototype.deleteContact = function(index) {
+	this.contacts.splice(index, 1);
+};
+
